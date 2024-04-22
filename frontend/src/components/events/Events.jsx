2d2a7b0d362ad962975f23d24c482600250f832e
Encoder012 <49637067+Encoder012@users.noreply.github.com>
2024-04-23 01:24:18 +0530

    made changes in events component

--- frontend/src/components/events/Events.jsx
+++ frontend/src/components/events/Events.jsx
@@ -3,6 +3,10 @@ import {Chrono} from "react-chrono";
 import {useEffect, useState} from "react";
 import axios from "axios";
 import {useNavigate} from "react-router-dom";
+import { FaMapLocationDot } from "react-icons/fa6";
+import { BsCalendar2DateFill } from "react-icons/bs";
+import { FcClock, FcCalendar, FcLandscape} from "react-icons/fc";
+// import { MdAccessTimeFilled } from "react-icons/md";
 
 
 export default function Events() {
@@ -29,26 +33,50 @@ export default function Events() {
     const onTimelineItemSelect = (data) => {
         setCard(eventData.find(i => i.id === data.cardTitle));
     }
+    console.log(eventData)
 
     return (
         <div className="eventSection">
             <h1>Events</h1>
-            <div onClick={()=> navigate(`/events/${card.id}`)} className="eventInfo">
-                <div className="eventImage">
-                    <img src={card.eventImages[0]} alt="image"></img>
+
+            <div className="eventList">
+                {/* <div className="dotted-line"></div> */}
+                {
+
+                    eventData.map((event) => (
+                        
+                        
+<div key={event.id} className="eventCard">
+
+                    <div className="circle">
+                        <div className="circleShape"></div>
+                    </div>
+                    <div style={{backgroundImage: "url("+ event.eventImages[0] + ")"}} className="eventImage">
+                        {/* <img src="" alt="image" /> */}
+                    </div>
+                    <div className="eventData">
+                        <h2><b>{event.name}</b></h2>
+                        <br />
+                        <h4>"Unleash the power of web3 and open source"</h4>
+                        <br />
+                        <div className="smallDets">
+                            <p className="dets"><FaMapLocationDot /> ADGIPS Auditorium</p>
+                            <p className="dets"><FcCalendar /> {
+                                new Date(event.date).toDateString()
+                            }</p>
+                            <p className="dets"><FcClock /> 12pm onwords</p>
+                        </div>
+                    </div>
                 </div>
+                    ))
+                }
+
             </div>
-            <div className="eventsTimeline">
-                <Chrono
-                    mode={"HORIZONTAL"}
-                    allowDynamicUpdate={true}
-                    items={eventData}
-                    cardPositionHorizontal={"TOP"}
-                    cardLess={true}
-                    timelinePointDimension={30}
-                    onItemSelected={onTimelineItemSelect}
-                />
-            </div>
+
         </div>
     );
 }
+
+
+
+
